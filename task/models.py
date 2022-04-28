b91e8d6744903b9435bbccf46ba104e1de95aacc
lodyne <lodgmtui@gmail.com>
2022-04-28 10:30:58 +0300

    change database to postgres

--- task/models.py
+++ task/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
+    id = models.IntegerField(primary_key=True)
     notes= models.CharField(max_length=100)
     date_created=models.DateField(default=timezone.now)
 
