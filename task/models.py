4f7d5a07882c589ee7d46ba13533aad0037447ad
lodyne <lodgmtui@gmail.com>
2022-04-28 10:46:04 +0300

    migrate changes to database

--- task/models.py
+++ task/models.py
@@ -3,10 +3,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
-    id = models.IntegerField(primary_key=True)
     notes= models.CharField(max_length=100)
     date_created=models.DateField(default=timezone.now)
 
     def __str__(self):
-        return self.notes
+        return self.id
 
